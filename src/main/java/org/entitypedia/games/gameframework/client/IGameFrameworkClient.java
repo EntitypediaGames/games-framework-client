50bd346c8fcb7c55b3937ef69efc44a851cfde29
autayeu <autayeu@468f3c8c-1912-47de-8909-53fc7abe8733>
2013-01-22 15:54:28 +0000

    common interface for clients
    git-svn-id: https://dev.entitypedia.org/svn/entitypedia/apps/word-games-framework-client/trunk@1009 468f3c8c-1912-47de-8909-53fc7abe8733

--- src/main/java/org/entitypedia/games/gameframework/client/IGameFrameworkClient.java
+++ src/main/java/org/entitypedia/games/gameframework/client/IGameFrameworkClient.java
@@ -1,9 +1,10 @@
 package org.entitypedia.games.gameframework.client;
 
+import org.entitypedia.games.common.client.IWordGameClient;
 import org.entitypedia.games.gameframework.common.api.IPlayerAPI;
 
 /**
  * @author <a rel="author" href="http://autayeu.com/">Aliaksandr Autayeu</a>
  */
-public interface IGameFrameworkClient extends IPlayerAPI {
+public interface IGameFrameworkClient extends IWordGameClient, IPlayerAPI {
 }
